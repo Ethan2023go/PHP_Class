e37d6dae7b07be596a98102f04209c3ff8fefcd0
Ethan <easonbest123@gmail.com>
2023-10-06 13:12:18 +0800

    巢狀

--- loop/loop_test.php
+++ loop/loop_test.php
@@ -30,12 +30,13 @@
         <li>1,3,5,7,9....n</li>
         <li>10,20,30,40...n</li>
         <li>1,3,5,7,11,13...n</li>
-        <li></li>
+
     </ul>
     <?php
-    $n=100;
-    for($i=1;$i<=n;$i+2){
-        echo $i;
+    
+    $n=30;
+    for($i=1;$i<=$n;$i=$i+2){
+        echo $i ;
         echo ",";
     }
     echo "<hr>";
@@ -46,7 +47,50 @@
 
     echo "<hr>";
     $a=83;
+    $flag=true;
+    for($i=2;$i<($a/2);$i++){
+        echo "$a 除以 $i 餘數".
+        ($a%$i);
+    
+    if (($a % $i) == 0) 
+        $flag=false;
+        echo "<br>";
+        break;
+    }
+    echo "<br>";
+    if($flag==true){
+        echo $a . "是質數";
+    }
+    else{
+        echo $a . "不是質數";
+    }
+   ?>
 
+    <h3>巢狀迴圈</h3>
+    <?php
+    $n=100;
+    $count=0;
+    for($j=3;$j<=$n;$j++){
+        $flag=true;
+    }
+    
+    for($i=2;$i<=($j/2);$i++){
+        $count++;
+    if(($j % $i) == 0) {
+        $flag=false;
+        break;
+    }
+    }
+
+    if($flag==true){
+        echo $j;
+        echo ",";
+    } else{
+        //echo $j . "不是質數";
+    }
+    echo "<br>";
+echo "一共執行了".$count."次的程式內容";
+?>
 
 
 </body>
