31915490151032a7790b7d231cf5061f673eeb3c
Ethan <easonbest123@gmail.com>
2023-10-24 10:16:02 +0800

    日期差異跨年

--- Time/s1024_time.php
+++ Time/s1024_time.php
@@ -21,5 +21,38 @@ $days=(strtotime($date2)-strtotime($date1))/(60*60*24);
 echo "<hr>";
 echo $date1.'到'.$date2."有".$days."天";
 // echo 最後要變成字串或數字 才可以接echo
+?>
+
+<h2>計算下次生日天數</h2>
+<?php
+// $date="1974-10-07";
+// $timestamp = mktime(9, 27, 0, 10, 24, 2023); 
+// echo $timestamp;
+// echo "<br>";
+// echo date("Y-m-d H:i:s",$timestamp);
+// $days1='2023-12-31';
+// echo "<br>";
+// $days=(strtotime($timestamp)-strtotime($date1))/(60*60*24);
+
+// echo $timestamp.'到'.$date1."有".$days."天";
+
+
+?>
+<h2>計算下次生日天數</h2>
+<?php
+$date="1974-10-07";
+$br=strtotime($date);
+$diff=strtotime(date("Y")."-".date("m-d",$br));
+$today=strtotime('now');
+if($diff>$today){
+    $days=($diff-$today)/(60*60*24);
+}else{
+    $diff=strtotime("+1 year",$diff);
+    $days=($diff-$today)/(60*60*24);
+}
+
+echo "距離下一次生日:".date("Y-m-d",$diff)."還有".floor($days)."天";
+
+
 
 ?>
