63aaec348d8e497df6b5e392ddc8364c9ba45cd2
Ethan <easonbest123@gmail.com>
2023-10-24 11:20:23 +0800

    日期+迴圈

--- Time/s1024_time.php
+++ Time/s1024_time.php
@@ -25,23 +25,8 @@ echo $date1.'到'.$date2."有".$days."天";
 
 <h2>計算下次生日天數</h2>
 <?php
-// $date="1974-10-07";
-// $timestamp = mktime(9, 27, 0, 10, 24, 2023); 
-// echo $timestamp;
-// echo "<br>";
-// echo date("Y-m-d H:i:s",$timestamp);
-// $days1='2023-12-31';
-// echo "<br>";
-// $days=(strtotime($timestamp)-strtotime($date1))/(60*60*24);
-
-// echo $timestamp.'到'.$date1."有".$days."天";
-
-
-?>
-<h2>計算下次生日天數</h2>
-<?php
 $date="1974-10-07";
-$br=strtotime($date);
+// $br=strtotime($date); 電腦生日日期瑪
 $diff=strtotime(date("Y")."-".date("m-d",$br));
 $today=strtotime('now');
 if($diff>$today){
@@ -53,6 +38,52 @@ if($diff>$today){
 
 echo "距離下一次生日:".date("Y-m-d",$diff)."還有".floor($days)."天";
 
+?>
+<h2>利用date()函式的格式化參數，完成以下的日期格式呈現</h2>
+2021/10/05
+10月5日 Tuesday
+2021-10-5 12:9:5
+2021-10-5 12:09:05
+今天是西元2021年10月5日 上班日(或假日)
+<br>
+<hr><br>
+<?php
+echo date("Y/m/d");
+echo "<br>";
+echo date("m月d日 星期一");
+echo "<br>";
+echo date("Y-m-d H:i:s");
+echo "<br>";
+// echo date("Y-m-d G:i:s");
+// echo "<br>";
 
+echo date("今天是西元Y年m月d日");
+if(date("N")<=5){
+    echo "上班日";
+}else{
+    echo "假日";
+}
+
+?>
+
+<h2>計算下次生日天數</h2>
+<ul>
+    <li>2021-10-04 星期一</li>
+    <li>2021-10-11 星期一</li>
+    <li>2021-10-18 星期一</li>
+    <li>2021-10-25 星期一</li>
+    <li>2021-11-01 星期一</li>
+</ul>
+<?php
+$todayWeek=date("N");
+$diff=1-$todayWeek;
+$lastMonday=strtotime("$diff days");
+$nextMonday=date("Y-m-d",strtotime("+1 week",$lastMonday));
+
+for($i=0;$i<5;$i++){
+    $nextMonday=date("Y-m-d l",strtotime("+1 week",strtotime($nextMonday)));
+    echo $nextMonday;
+    echo "<br>";
+}
 
 ?>
