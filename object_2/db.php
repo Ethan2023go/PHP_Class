5822847a1d0e505d2d23e0e859790d04dd5e53e8
Ethan <easonbest123@gmail.com>
2023-11-24 16:18:40 +0800

    簡化重複部分

--- object_2/db.php
+++ object_2/db.php
@@ -86,11 +86,27 @@ function find($id)
 
 function save($array){
     if(isset($array['id'])){
-    $this->update($array['id'],
-    $array);
-   }else{
-       $this->insert($array);
+        $sql = "update `$this->table` set ";
+
+        if (!empty($cols)) {
+            foreach ($cols as $col => $value) {
+                $tmp[] = "`$col`='$value'";
+            }
+        } else {
+            echo "錯誤:缺少要編輯的欄位陣列";
+        }
+    
+        $sql .= join(",", $tmp);
+        $sql .= " where `id`='{$array['id']}'";
+    }else{
+        $sql = "insert into `$this->table` ";
+        $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+        $vals = "('" . join("','", $array) . "')";
+    
+        $sql = $sql . $cols . " values " . $vals;
     }
+
+    return $this->pdo->exec($sql);
 }
     
 
